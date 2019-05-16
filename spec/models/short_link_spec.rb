178e0c66d3ac0fb305a5d2b837ef53f389c63d9b
Michael Rapp <michael@Michaels-iMac.local>
2019-05-16 15:47:23 -0700

    ShortLink validates original_url presence

--- spec/models/short_link_spec.rb
+++ spec/models/short_link_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ShortLink, type: :model do
+  context 'validation tests' do
+    it 'ensures url is present' do
+      short_link = ShortLink.new().save
+      expect(short_link).to eq false
+    end
+  end
 end
